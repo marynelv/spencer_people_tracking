a90c00f8b7640c96f14ed239d3412aebf74e665a
Marynel Vazquez <marynel.vazquez@disneyresearch.com>
2015-09-23 10:00:55 -0400

    parameterized a bit more generic_detector_node.cpp

--- detection/laser_detectors/srl_laser_detectors/src/srl_laser_detectors/ros/nodes/generic_detector_node.cpp
+++ detection/laser_detectors/srl_laser_detectors/src/srl_laser_detectors/ros/nodes/generic_detector_node.cpp
@@ -16,8 +16,15 @@ int main(int argc, char **argv)
     DetectorType type; privateHandle.param<std::string>("type", type, "");
     boost::shared_ptr<Detector> detector = DetectorFactory::createDetector(type, nodeHandle, privateHandle);
 
+    // Other params
+    std::string laser_topic = "laser";
+    privateHandle.param<std::string>("laser_topic", laser_topic, laser_topic);
+
+    std::string segmentation_topic = "segmentation_topic";
+    privateHandle.param<std::string>("segmentation_topic", segmentation_topic, segmentation_topic);
+
     // Subscribe to laser scans and segmentations
     ROSInterface rosInterface(nodeHandle, privateHandle);
-    rosInterface.connect(detector.get());
+    rosInterface.connect(detector.get(), laser_topic, segmentation_topic);
     ros::spin();
 }
